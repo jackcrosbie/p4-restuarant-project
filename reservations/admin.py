53521392b67a3438d9b134e94e9d37dbdd1df550
jackcrosbie <jack.crosbie@mycit.ie>
2022-02-02 23:25:03 +0000

    inital admin panel set up with reservations

--- reservations/admin.py
+++ reservations/admin.py
@@ -3,4 +3,8 @@ from .models import Reservations
 
 # Register your models here.
 
-admin.site.register(Reservations)
+@admin.register(Reservations)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'time', 'number_of_party')
+    list_filter = ('name', 'date')
+    search_fields = ['name', 'date', 'time', 'email']
