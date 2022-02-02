2253eea4de5cc4908c309c61ee1e94a417f0738b
jackcrosbie <jack.crosbie@mycit.ie>
2022-02-02 22:44:45 +0000

    add models for reservations

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+class Reservations(models.Model):
+    name = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+    email = models.EmailField()
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_party = models.IntegerField()
+
+    def __str__(self):
+        return self.name
